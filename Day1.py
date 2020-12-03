880b82286da952408937a7b0702465a18fed3443
Dustin Karp <dustinkarp52@gmail.com>
2020-12-03 15:57:54 -0500

    Q1P1

---
+++ Day1.py
@@ -0,0 +1,17 @@
+file = open("sums.txt")
+
+lines = file.readlines()
+
+numset = {"0"}
+
+#Part 1: find 2 nubers that sum to 2020
+for line in lines:
+    numset.add(int(line))
+    if ((2020 - int(line)) in numset):
+        print(line, 2020 - int(line))
+        product = int(line) * (2020 - int(line))
+        print("product: " + str(product))
+
+#Part 2: find 3 numbers that sum to 2020
+for line in lines:
+    numset.add
