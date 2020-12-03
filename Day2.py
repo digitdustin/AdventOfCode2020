2fa2ec39eaf8ea30a7b8452a236eaa4fd56da245
Dustin Karp <dustinkarp52@gmail.com>
2020-12-03 18:42:46 -0500

    Day2

--- Day2.py
+++ Day2.py
@@ -4,11 +4,22 @@ lines = file.read().splitlines()
 
 count = 0
 
+# part 1: toboggan password rule 1
 for line in lines:
     vals = line.split(" ") # all input values
     limits = vals[0].split("-") # upper and lower lim for char
     char = vals[1][0] # character to check
     if (vals[2].count(char) >= int(limits[0]) and vals[2].count(char) <= int(limits[1])):
         count += 1
+print("old system: " + str(count))
 
-print(count)
+count = 0
+# part 2: toboggan password rule 2
+for line in lines:
+    vals = line.split(" ") # all input values
+    limits = vals[0].split("-") # upper and lower lim for char
+    char = vals[1][0] # character to check
+    if ((vals[2][int(limits[0]) - 1] == char and vals[2][int(limits[1]) - 1] != char) or (vals[2][int(limits[0]) - 1] != char and vals[2][int(limits[1]) - 1] == char)):
+        count += 1
+
+print("new system: " + str(count))
